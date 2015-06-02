536bdd40b4d2105836868dd5f8db5471b5b62992
Yarique Belgorodsky <spec1al.yarik@gmail.com>
2015-06-03 00:10:17 +0300

    Zooming/Opening image in shchukin and some team projects changes

--- shchukin/src/mainwindow.h
+++ shchukin/src/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QScrollBar>
 
 class Storage;
 
@@ -16,8 +18,22 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private slots:
+    void on_openButton_clicked();
+
+    void on_zoomInButton_clicked();
+
+    void on_zoomOutButton_clicked();
+
+    void on_zoomAutoButton_clicked();
+
 private:
     Storage *storage_;
+    QSize storage_size_;
+    void openImage();
+    void autoZoom();
+    void zoomChange(bool in);
+    QPixmap _image;
 
 private:
     Ui::MainWindow *ui;
