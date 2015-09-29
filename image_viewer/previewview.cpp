7550601a608f5d0c8b7f3a5c63ff1b914a2ee034
0noda <0n0da@mail.ru>
2015-09-30 02:22:46 +0300

    upd

---
+++ image_viewer/previewview.cpp
@@ -0,0 +1,5 @@
+#include "previewview.h"
+
+PreviewView::PreviewView()
+{
+}
---
+++ image_viewer/previewview.h
@@ -0,0 +1,18 @@
+#ifndef PREVIEWVIEW_H
+#define PREVIEWVIEW_H
+
+#include <QGraphicsView>
+
+class PreviewView : public QGraphicsView
+{
+    Q_OBJECT
+public:
+    PreviewView();
+
+signals:
+
+public slots:
+
+};
+
+#endif // PREVIEWVIEW_H
